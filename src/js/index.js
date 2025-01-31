0302a2bb1d0cc16df2002e4252bb4691560ea6e1
Yaroslav <yarobondmail@gmail.com>
2025-01-31 16:11:30 +0000

    build: set correct routes

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@ import { Model } from './model/index.js';
 import { View } from './view/index.js';
 import { Controller } from './controller/index.js';
 
-const model = new Model('../assets/data/data.json');
+const model = new Model('./src/assets/data/data.json');
 const view = new View();
 new Controller(model, view);
